5ce5222df6a22ca63758af88f3c1be8dc9d547d1
Tom Edge <tom.edge@autotrader.co.uk>
2025-01-14 20:18:02 +0000

    http: rename ignore_http_11_upgrade to ignore_http_11_tls_upgrade for clarity
    Signed-off-by: Tom Edge <tom.edge@autotrader.co.uk>

--- envoy/http/codec.h
+++ envoy/http/codec.h
@@ -497,7 +497,8 @@ struct Http1Settings {
   // headers set. By default such messages are rejected, but if option is enabled - Envoy will
   // remove Content-Length header and process message.
   bool allow_chunked_length_{false};
-  // Remove HTTP/1.1 TLS Upgrade header from message and process. By default such messages are rejected
+  // Remove HTTP/1.1 TLS Upgrade header from message and process. By default such messages are
+  // rejected
   bool ignore_http_11_tls_upgrade_{false};
 
   enum class HeaderKeyFormat {
